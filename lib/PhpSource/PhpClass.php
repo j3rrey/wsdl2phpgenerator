7f3dd2db614e87979f44e613b27eef6e438e2095
Jeremy Bruns <bruns.jeremy@gmail.com>
2018-11-19 19:10:52 +0100

    fixed parameter must be an array or an object that implements countable error on php7.2

--- lib/PhpSource/PhpClass.php
+++ lib/PhpSource/PhpClass.php
@@ -153,7 +153,7 @@ class PhpClass extends PhpElement
             $ret .= ' extends ' . $this->extends;
         }
 
-        if (count($this->implements) > 0) {
+        if (count((array)$this->implements) > 0) {
             $ret .= ' implements ' . implode(', ', $this->implements);
         }
 
